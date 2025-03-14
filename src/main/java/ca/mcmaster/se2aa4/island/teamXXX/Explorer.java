ea5dc0e8f12b9d57d66933ecb230b150a86aaf06
Soham <sohamhajariwala@outlook.com>
2025-03-14 17:16:21 -0400

    updated explorer

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -1,51 +1,63 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
 import java.io.StringReader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-public class Explorer implements IExplorerRaid {
+import eu.ace_design.island.bot.IExplorerRaid;
 
+public class Explorer implements IExplorerRaid {
     private final Logger logger = LogManager.getLogger();
+    private DroneState droneState;
+    private boolean foundGround = false;
 
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Initialization info:\n {}",info.toString(2));
+        logger.info("** Initialization info:\n {}", info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
+
+        droneState = new DroneState();
+        droneState.initializeDrone(direction, batteryLevel);
     }
 
     @Override
     public String takeDecision() {
-        JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
-        logger.info("** Decision: {}",decision.toString());
-        return decision.toString();
+        if (!foundGround) {
+            logger.info("Scanning for ground...");
+            return CreateCommand.newScanCommand().toString();
+        } else {
+            logger.info("Ground found, returning to base...");
+            return CreateCommand.newStopCommand().toString();
+        }
     }
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n"+response.toString(2));
+        logger.info("** Response received:\n{}", response.toString(2));
         Integer cost = response.getInt("cost");
-        logger.info("The cost of the action was {}", cost);
-        String status = response.getString("status");
-        logger.info("The status of the drone is {}", status);
-        JSONObject extraInfo = response.getJSONObject("extras");
-        logger.info("Additional information received: {}", extraInfo);
+        droneState.updateBatteryLevel(cost);
+        logger.info("Current battery level: {}", droneState.getBatteryLevel());
+
+        if (response.has("extras") && response.getJSONObject("extras").has("biomes")) {
+            String biome = response.getJSONObject("extras").getJSONArray("biomes").getString(0);
+            if (!biome.equals("OCEAN")) {
+                logger.info("Ground detected: {}", biome);
+                foundGround = true;
+            }
+        }
     }
 
     @Override
     public String deliverFinalReport() {
-        return "no creek found";
+        return "Mission Completed: Ground Detected and Returned to Base.";
     }
-
 }
