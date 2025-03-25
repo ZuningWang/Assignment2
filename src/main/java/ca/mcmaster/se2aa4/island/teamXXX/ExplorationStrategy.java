8006f4f5bd4ca89290487ff9a9fd4ee652efcea3
kathaerina <yonge2@mcmaster.ca>
2025-03-24 21:19:08 -0400

    fix: commenting, logger calls, refactored SweepStrategy

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/ExplorationStrategy.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/ExplorationStrategy.java
@@ -2,7 +2,7 @@ package ca.mcmaster.se2aa4.island.teamXXX;
 
 import org.json.JSONObject;
 
-public interface ExplorationStrategy {
+public interface ExplorationStrategy { // interface for strategies that must implement the start() and explore() methods
 
     public void start(DroneState droneState, CommandList commandList);
     public void explore(DroneState droneState, CommandList commandList, JSONObject extraInfo);
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SweepStrategy.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SweepStrategy.java
@@ -12,12 +12,12 @@ public class SweepStrategy implements ExplorationStrategy {
     private final Logger logger = LogManager.getLogger();
 
     private int stage = 0;
-    private int hasOcean = 0;
     private boolean hasLand = false;
     private int rangeX = 0;
     private int rangeY = 0;
     private int startX = 0;
 
+    // starts by determining bound range of x
     public void start(DroneState droneState, CommandList commandList) {
         commandList.addCommand(CreateCommand.newEchoCommand("E"));
     }
@@ -25,61 +25,59 @@ public class SweepStrategy implements ExplorationStrategy {
     public void explore(DroneState droneState, CommandList commandList, JSONObject extraInfo) {
         switch (stage) {
             case 0:
+
                 if (rangeX == 0) {
+                    // updates bound range of x, sends command to check bound range of y
                     rangeX = extraInfo.getInt("range");
-                    logger.trace("THE RANGE OF X IS: {}", rangeX);
+                    logger.info("THE RANGE OF X IS: {}", rangeX);
                     commandList.addCommand(CreateCommand.newEchoCommand("S"));
-                    startX++;
                 } else if (rangeY == 0) {
+                    // updates bound range of y, send command to start search
                     rangeY = extraInfo.getInt("range");
-                    logger.trace("THE RANGE OF Y IS: {}", rangeX);
+                    logger.info("THE RANGE OF Y IS: {}", rangeY);
                     commandList.addCommand(CreateCommand.newFlyCommand());
                     commandList.addCommand(CreateCommand.newEchoCommand("S"));
                     startX++;
                 } else if (extraInfo.has("found")) {
                     String echoResult = extraInfo.getString("found");
-                    logger.trace("ECHO RESULT HERE: {}", echoResult);
+                    logger.trace("ECHO RESULT: {}", echoResult);
+
                     if (echoResult.equals("OUT_OF_RANGE")) {
-                        logger.trace("ADDING MORE INSTRUCTIONS");
+                        // no land found, continue east
                         commandList.addCommand(CreateCommand.newFlyCommand());
                         commandList.addCommand(CreateCommand.newEchoCommand("S"));
                         startX++;
                     } else {
+                        // land found, perform 90 degree turn maneuver
                         int yDistance = extraInfo.getInt("range");
                         commandList.addCommand(CreateCommand.newFlyCommand());
                         commandList.addCommand(CreateCommand.newHeadingCommand("S"));
                         commandList.addCommand(CreateCommand.newHeadingCommand("W"));
                         commandList.addCommand(CreateCommand.newHeadingCommand("S"));
+                        // fly distance to land
                         for (int i = 0; i < yDistance - 2; i++) {
                             commandList.addCommand(CreateCommand.newFlyCommand());
                         }
                         commandList.addCommand(CreateCommand.newScanCommand());
                         stage++;
-                        logger.trace("MOVING ONTO NEXT STAGE:{}", stage);
+                        logger.trace("MOVING ONTO NEXT STAGE: {}", stage);
                         logger.info("NUMBER OF COMMANDS: {}", commandList.numCommands());
-
                     }
+
                 } else if (extraInfo.has("biomes")) {
-                    String scanResult = extraInfo.get("biomes").toString();
-                    logger.info("SCAN RESULT HERE: {}", scanResult);
+                    JSONArray scanResult = extraInfo.getJSONArray("biomes");
 
-//add interested sites
-                    if(!extraInfo.get("creeks").toString().equals("[]")){
-                        TargetFound.getInstance().addCreeksFound(extraInfo.get("creeks").toString(),"Creek", droneState.getPositionX(), droneState.getPositionY());
-                    }
-                    if(!extraInfo.get("sites").toString().equals("[]")){
-                        TargetFound.getInstance().addSiteFound(extraInfo.get("sites").toString(),"Site", droneState.getPositionX(), droneState.getPositionY());
+                    for (int i = 0; i < scanResult.length(); i++) {
+                        String scanResultElement = scanResult.getString(i);
+                        logger.trace("SCAN RESULT {}: {}", i+1, scanResultElement); 
                     }
 
                 }
                 break;
-            case 1:
-                
-                // if ((droneState.getBatteryLevel() < 7000)) {
-                //     commandList.emptyCommands();
-                //     commandList.addCommand(CreateCommand.newStopCommand());
-                // }
 
+            case 1:
+
+                // if drone is going to fly out of range, return to base
                 if ((droneState.getPositionY() < 2 && droneState.getPositionX() > startX + 2) || droneState.getPositionX() > rangeX - 2 || droneState.getPositionY() > rangeY) {
                     commandList.emptyCommands();
                     logger.warn("STOPPING BECAUSE OF RANGE");
@@ -88,9 +86,10 @@ public class SweepStrategy implements ExplorationStrategy {
 
                 if (extraInfo.has("found")) {
                     String echoResult = extraInfo.getString("found");
-                    logger.info("ECHO RESULT HERE: {}", echoResult);
+                    logger.trace("ECHO RESULT: {}", echoResult);
                 } else if (extraInfo.has("biomes")) {
-//add interested sites
+
+                    // add points of interest
                     if(!extraInfo.get("creeks").toString().equals("[]")){
                         TargetFound.getInstance().addCreeksFound(extraInfo.get("creeks").toString(),"Creek", droneState.getPositionX(), droneState.getPositionY());
                     }
@@ -102,50 +101,25 @@ public class SweepStrategy implements ExplorationStrategy {
                     
                     if (commandList.numCommands() == 0) {
 
+                        // sets variable based on scan results
                         for (int i = 0; i < scanResult.length(); i++) {
                             String scanResultElement = scanResult.getString(i);
                             if (!scanResultElement.equals("OCEAN")) {
-                                hasOcean = 0;
                                 hasLand = true;
                             }
                         }
-
-                        if (hasOcean == 2) {
-                            logger.warn("STOPPING BECAUSE OF DOUBLE OCEAN");
-                            commandList.addCommand(CreateCommand.newStopCommand());
-                        } else if (hasLand == true && scanResult.length() == 1 && scanResult.getString(0).equals("OCEAN")) {
-
-                            switch (droneState.getDirection()) {
-                                case NORTH:
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("E"));
-                                    commandList.addCommand(CreateCommand.newFlyCommand());
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("S"));
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("W"));
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("S"));
-                                    commandList.addCommand(CreateCommand.newScanCommand());
-                                    hasLand = false;
-                                    hasOcean++;
-                                    break;
-                                case EAST:
-                                    break;
-                                case SOUTH:
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("E"));
-                                    commandList.addCommand(CreateCommand.newFlyCommand());
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("N"));
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("W"));
-                                    commandList.addCommand(CreateCommand.newHeadingCommand("N"));
-                                    commandList.addCommand(CreateCommand.newScanCommand());
-                                    hasLand = false;
-                                    hasOcean++;
-                                    break;
-                                case WEST:
-                                    break;
-                                default:
-                                    commandList.addCommand(CreateCommand.newStopCommand());
-                                    logger.warn("shouldnt ever happen");
-                                    break;
-                            }
+                        
+                        if (hasLand == true && scanResult.length() == 1 && scanResult.getString(0).equals("OCEAN")) {
+                            // perform 180 degree turn maneuver
+                            commandList.addCommand(CreateCommand.newHeadingCommand("E"));
+                            commandList.addCommand(CreateCommand.newFlyCommand());
+                            commandList.addCommand(CreateCommand.newHeadingCommand(droneState.getDirection().backwards()));
+                            commandList.addCommand(CreateCommand.newHeadingCommand("W"));
+                            commandList.addCommand(CreateCommand.newHeadingCommand(droneState.getDirection().backwards()));
+                            commandList.addCommand(CreateCommand.newScanCommand());
+                            hasLand = false;
                         } else {
+                            //fly through land
                             commandList.addCommand(CreateCommand.newFlyCommand());
                             commandList.addCommand(CreateCommand.newScanCommand());
                         }
@@ -153,9 +127,10 @@ public class SweepStrategy implements ExplorationStrategy {
 
                     for (int i = 0; i < scanResult.length(); i++) {
                         String scanResultElement = scanResult.getString(i);
-                        logger.info("SCAN RESULT {}: {}", i+1, scanResultElement); 
+                        logger.trace("SCAN RESULT {}: {}", i+1, scanResultElement); 
                     }
 
+                    // stop after finding first creek
                     // JSONArray creek = extraInfo.getJSONArray("creeks");
                     // if (creek.length() != 0) {
                     //     logger.info("CREEK: {}",creek.getString(0));
@@ -164,6 +139,7 @@ public class SweepStrategy implements ExplorationStrategy {
                     //     commandList.addCommand(CreateCommand.newStopCommand());
                     // }
 
+                    // stop after finding site
                     // JSONArray site = extraInfo.getJSONArray("sites");
                     // if (site.length() != 0) {
                     //     logger.info("SITE: {}",site.getString(0));
@@ -174,8 +150,6 @@ public class SweepStrategy implements ExplorationStrategy {
 
                 }
                 
-                break;
-            default:
                 break;
         }
     }
